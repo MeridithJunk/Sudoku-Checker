a5b465971077d4c94a88079f8283bf3bd45d4158
Meridith <meridith.junk@gmail.com>
2022-04-11 12:29:03 -0500

    initialize

---
+++ SudokuChecker/SudokuCheckerTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace SudokuCheckerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
