28d51263b29f94e7832b742dd90ca1c815ff6584
Meridith <meridith.junk@gmail.com>
2022-04-12 13:36:19 -0500

    vertical solution

--- SudokuChecker/SudokuChecker/Sudoku.cs
+++ SudokuChecker/SudokuChecker/Sudoku.cs
@@ -4,8 +4,37 @@ public class Sudoku
 {
     public bool Checker(int[,] sudoku)
     {
+        var verticalResult = VerticalRowChecker(sudoku);
+        var horizontalResult = HorizontalRowChecker(sudoku);
+
+        return verticalResult && horizontalResult;
+    }
+
+    private bool VerticalRowChecker(int[,] sudoku)
+    {
         var result = true;
-        for(var row = 0; row <= 8; row++)
+        for (var column = 0; column <= 8; column++)
+        {
+            var verticalRowChecker = new HashSet<int>(); 
+            for(var row = 0; row <= 8; row++)
+            {
+                var number = sudoku[row, column];
+                verticalRowChecker.Add(number);
+            }
+            
+            if (verticalRowChecker.Count != 9)
+            {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    private static bool HorizontalRowChecker(int[,] sudoku)
+    {
+        var result = true;
+        for (var row = 0; row <= 8; row++)
         {
             var horizontalRowChecker = new HashSet<int>();
             for (var column = 0; column <= 8; column++)
