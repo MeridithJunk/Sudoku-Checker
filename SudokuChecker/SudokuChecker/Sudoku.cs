d40b373c4ebca341f1ee069cd617412005bfecf1
Meridith <meridith.junk@gmail.com>
2022-04-12 13:19:40 -0500

    vertical row checker failed test

--- SudokuChecker/SudokuChecker/Sudoku.cs
+++ SudokuChecker/SudokuChecker/Sudoku.cs
@@ -7,14 +7,14 @@ public class Sudoku
         var result = true;
         for(var row = 0; row <= 8; row++)
         {
-            var rowChecker = new HashSet<int>();
+            var horizontalRowChecker = new HashSet<int>();
             for (var column = 0; column <= 8; column++)
             {
                 var number = sudoku[row, column];
-                rowChecker.Add(number);
+                horizontalRowChecker.Add(number);
             }
 
-            if (rowChecker.Count != 9)
+            if (horizontalRowChecker.Count != 9)
             {
                 result = false;
             }
