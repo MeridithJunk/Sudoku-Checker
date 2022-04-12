aee3f6318edc0dee7e53c56d63aa29e7da01ee51
Meridith <meridith.junk@gmail.com>
2022-04-12 13:10:47 -0500

    solves by rows

--- SudokuChecker/SudokuChecker/Sudoku.cs
+++ SudokuChecker/SudokuChecker/Sudoku.cs
@@ -2,9 +2,24 @@ namespace SudokuChecker;
 
 public class Sudoku
 {
-    public bool Checker(int[] sudoku)
+    public bool Checker(int[,] sudoku)
     {
-        var set = new HashSet<int>(sudoku);
-        return set.Count == 9;
+        var result = true;
+        for(var row = 0; row <= 8; row++)
+        {
+            var rowChecker = new HashSet<int>();
+            for (var column = 0; column <= 8; column++)
+            {
+                var number = sudoku[row, column];
+                rowChecker.Add(number);
+            }
+
+            if (rowChecker.Count != 9)
+            {
+                result = false;
+            }
+        }
+
+        return result;
     }
 }
