53817c7855ce07ff0be4fd80af4c760513f0f6f1
Meridith <meridith.junk@gmail.com>
2022-04-17 17:35:56 -0500

    adds box checking, the 3x3 rows

--- SudokuChecker/SudokuChecker/Sudoku.cs
+++ SudokuChecker/SudokuChecker/Sudoku.cs
@@ -4,7 +4,44 @@ public class Sudoku
 {
     public bool Checker(int[,] sudoku)
     {
-        return HorizontalRowChecker(sudoku);
+        return blockChecker(sudoku) && HorizontalRowChecker(sudoku);
+    }
+
+    private bool blockChecker(int[,] sudoku)
+    {
+        var boxOne = IndividualBlockChecker(sudoku, 0,2,0,2);
+        var boxTwo = IndividualBlockChecker(sudoku, 0,2,3,5);
+        var boxThree = IndividualBlockChecker(sudoku, 0,2,6,8);
+        var boxFour = IndividualBlockChecker(sudoku, 3,5,0,2);
+        var boxFive = IndividualBlockChecker(sudoku, 3,5,3,5);
+        var boxSix = IndividualBlockChecker(sudoku, 3,5,6,8);
+        var boxSeven = IndividualBlockChecker(sudoku, 6,8,0,2);
+        var boxEight = IndividualBlockChecker(sudoku, 6,8,3,5);
+        var boxNine = IndividualBlockChecker(sudoku, 6,8,6,8);
+        return boxOne && boxTwo && boxThree 
+               && boxFour && boxFive && boxSix
+               && boxSeven && boxEight && boxNine;
+    }
+
+    private static bool IndividualBlockChecker(int[,] sudoku, int rowsStart, int rowEnd, int colStart, int colEnd)
+    {
+        var result = true;
+        var blockChecker = new HashSet<int>();
+        for (var row = rowsStart; row <= rowEnd; row++)
+        {
+            for (var column = colStart; column <= colEnd; column++)
+            {
+                var number = sudoku[row, column];
+                blockChecker.Add(number);
+            }
+        }
+
+        if (blockChecker.Count != 9)
+        {
+            result = false;
+        }
+
+        return result;
     }
 
     private static bool HorizontalRowChecker(int[,] sudoku)
